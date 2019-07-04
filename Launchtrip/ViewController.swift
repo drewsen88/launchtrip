1eb6af714e344ca26e26ed28451ba62ca820a55f
Drew Sen <drew@launchtrip.com>
2019-07-04 11:31:16 -0700

    Initial commit

---
+++ Launchtrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Launchtrip
+//
+//  Created by Drew Sen on 2019-06-26.
+//  Copyright © 2019 Drew Sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
